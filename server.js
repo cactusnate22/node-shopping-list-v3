73f5c4ac84aebb8281709478d02dc428b360a95a
Nathan Ash <nash311@gmail.com>
2017-12-06 09:49:22 -0500

    fixed line 84...DELETE working now

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/recipes', jsonParser, (req, res) => {
 
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
-  console.log(`Deleted recipe item` \`${req.params.id}\``);
+  console.log(`Deleted recipe item \`${req.params.id}\``);
   res.status(204).end();
 });
 
